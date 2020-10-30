e6f9e48e3152accde8f827698411c4f3c2ee4e07
Erdem Özkök <51801996+erdemozk@users.noreply.github.com>
2020-10-30 23:50:09 +0300

    Basic Addition

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!";
+    int deger1 = 10;
+    int a, b;
+
+    cout << "Welcome Everyone\n\tto\n\tC++" << endl;
+    cout << "How are you today?" << endl;
+    cout << deger1 << endl;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    int top = a + b;
+    cout << "Toplam: " << top << endl;
     return 0;
 }
