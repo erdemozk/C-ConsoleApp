c3e736a95521a549cde3125321d9246a7c12d7b9
Erdem Özkök <51801996+erdemozk@users.noreply.github.com>
2020-11-01 00:10:39 +0300

    If/Else/While/For/DoWhile Condition

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int fourOperation() {
     int deger1 = 10;
     int a, b;
 
@@ -39,5 +40,85 @@ int main() {
     cout << "Çıkarma: " << cik << endl;
     cout << "Bölme: " << böl << endl;
     cout << "Mod: " << mod << endl;
+
+};
+
+int ifElseCondition() {
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+
+    if (x > 5) {
+        cout << "X 5'ten büyük!\n";
+    }
+
+    if (x >= 5) {
+        cout << "X 5'ten büyük ya da 5'e eşit!\n";
+    }
+
+    if (x <= 5) {
+        cout << "X 5'ten küçük ya da 5'e eşit!\n";
+    }
+
+    if (x == 5) {
+        cout << "X 5'e eşit!\n";
+    }
+
+    if (x != 5) {
+        cout << "X 5'e eşit değil!\n";
+    }
+    else {
+        cout << "X bir tam sayı değil!";
+    }
+
+    return 0;
+};
+
+int whileCondition() {
+    int x = 5;
+
+    while (x > 0) {
+        x--;
+        cout << x << endl;
+    }
+};
+
+int forCondition() {
+    for (int x = 0; x < 10; x++) {
+        cout << x << endl;
+    }
+    for (int y = 0; y < 10; y += 5) {
+        cout << y << endl;
+    }
+};
+
+int doWhileCondition() {
+    int x = 0;
+    do {
+        cout << x << "\t";
+        x++;
+    } while (x < 10);
+    int y = 35;
+    do {
+        cout << y;
+        y++;
+    } while (x < 5);
+};
+
+int infinity() {
+    int x = 5;
+    do {
+        cout << x << "\t\n";
+        x++;
+    } while (x > 0);
+};
+
+int main() {
+    //fourOperation();
+    //ifElseCondition();
+    //whileCondition();
+    //forCondition();
+    //doWhileCondition();
+    //infinity();
     return 0;
 }
